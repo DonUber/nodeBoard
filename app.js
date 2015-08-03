c21fb0cd2c1eb2ba32c600f92d372f2ff838252c
DonUber <donuber36@gmail.com>
2015-08-03 12:23:09 +0300

    Added some documentation

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 /*
  app.js
- TODO: Game result handling
- TODO: Extract further information from console output
- TODO: Implementation of tournaments and more players
  */
 
 var ai = {
@@ -47,11 +44,15 @@ var ai = {
     },
     go : function (current_game, callback){
         try{
+            // Start up child process
             var child = require('child_process').spawn(this.path);
             var response = "";
+            // Write fen to UCI engine
             child.stdin.write("position fen " + current_game.board.fen() + "\n");
+            // Get starting moment
             var start = new Date();
             var end;
+            // Send go command with parameters
             if(current_game.mode.toString().indexOf("depth") > -1){
                 child.stdin.write("go depth " + current_game.depth + "\n");
             }else if(current_game.mode.toString().indexOf("movetime") > -1){
@@ -59,9 +60,10 @@ var ai = {
             }else if(current_game.mode.toString().indexOf("time") > -1){
                 child.stdin.write("go btime " + current_game.btime + " wtime " + current_game.wtime + "\n");
             }
-
+            // Receive the returned data
             child.stdout.on('data', function(data) {
                 response = data.toString();
+                // Check if there is already a bestmove in the data
                 if(response.indexOf("bestmove") > -1){
                     end = new Date() - start;
                     console.log('Info: turn ' + current_game.board.turn());
@@ -72,6 +74,7 @@ var ai = {
                         current_game.btime = current_game.btime - end;
                         console.log('Info: btime ' + current_game.btime);
                     }
+                    //Extract bestmove from the data
                     var arr_response = response.split("\n");
                     var response_part;
                     for(response_part in arr_response){
@@ -80,6 +83,7 @@ var ai = {
                             try{
                                 child.stdin.write("quit\n");
                             }catch(err){}
+                            // Callback
                             callback(arr_response_parts[1]);
                             return 1;
                         }
@@ -97,6 +101,7 @@ var ai = {
 };
 
 function createGame(p1, p2, config){
+    // Create game object and load data from config
     var game = {
         id : 0,
         gameName : "",
@@ -145,17 +150,19 @@ function gameLoop(){
 }
 
 function next(result) {
+    // To next round if still rounds to go
     if(current_game.current_round < current_game.total_rounds){
         round = current_game.current_round + 1;
-        current_game.board.reset();
         play(round);
     }
 }
 
 var move_callback = function(bestmove){
     console.log('Info: move ' + bestmove);
+    // Extract info from bestmove response
     var from  = bestmove.slice(0,2);
     var to = bestmove.slice(2,4);
+    // Check if there is a promotion
     var promo = "";
     if(bestmove.length == 5) {
         promo = '='+bestmove[4].toUpperCase();
@@ -163,6 +170,7 @@ var move_callback = function(bestmove){
     var move_error = new Error('MoveFailed');
     var ret = null;
     try{
+        // Verify if it is a castling move
         if (bestmove == 'e1g1' && current_game.board.get(from).type == 'k'){
             ret = current_game.board.move('O-O');
         }else if (bestmove == 'e1c1' && current_game.board.get(from).type == 'k'){
@@ -172,6 +180,8 @@ var move_callback = function(bestmove){
         }else if (bestmove == 'e8c8' && current_game.board.get(from).type == 'k'){
             ret = current_game.board.move('O-O-O');
         }else if(current_game.board.get(from).type == 'p'){
+            // Pawn moves
+            // Verify if it is a en passant capture
             var fen = current_game.board.fen();
             var enpas = fen.split(" ")[3];
             if(current_game.board.get(to) == null && to != enpas){
@@ -180,26 +190,32 @@ var move_callback = function(bestmove){
                 ret = current_game.board.move(bestmove.slice(0,1) + 'x' + to + promo);
             }
         }else {
+            // Other pieces
             if (current_game.board.get(to) == null) {
+                // Non-capture moves
                 ret = current_game.board.move(current_game.board.get(from).type.toUpperCase() + to);
                 if (ret == null) {
                     ret = current_game.board.move(current_game.board.get(from).type.toUpperCase() + bestmove.slice(0, 1) + to);
                 }
             } else {
+                // Capture moves
                 ret = current_game.board.move(current_game.board.get(from).type.toUpperCase() + 'x' + to);
                 if (ret == null) {
                     ret = current_game.board.move(current_game.board.get(from).type.toUpperCase() + bestmove.slice(0, 1) + 'x' + to );
                 }
             }
         }
+        // Error handling
         if (ret == null) {
             throw move_error;;
         }
     }catch(err){
         console.log("Error: " + err);
     }
+    // Next turn
     current_game.turn = current_game.turn + 1;
     console.log('Info: fen ' + current_game.board.fen());
+    // Check if and how the game ended and add it to PGN header
     if(current_game.board.game_over() == true){
         var result = 'd';
         if(current_game.board.in_checkmate() == true && current_game.board.turn() == 'b'){
@@ -220,7 +236,9 @@ var move_callback = function(bestmove){
             console.log("Info: end threefold");
             current_game.board.header('Result', '1/2-1/2');
         }
+        // Stop the game
         current_game.running = 0;
+        // Save the PGN file
         if(config.options.save_pgn){
             var fname = current_game.gameName + '-' + date.format('DDMMYYYY') + '-' + current_game.current_round + '-' + current_game.id + '.pgn';
             fs.writeFile(fname, current_game.board.pgn(), function(err) {
@@ -228,32 +246,50 @@ var move_callback = function(bestmove){
                     return console.log(err);
                 }
                 console.log("Info: pgn saved "+fname);
+                // To next game
                 next(result);
             });
         }
     }else if(current_game.running == 1){
+        // Callback to gameloop
         gameLoop();
     }
 };
 
 function play(round){
+    // Create game object
     current_game = createGame(p1, p2, config);
+    // Generate game ID
     current_game.id = random(1000,9999);
+    // Create chess.js object
     current_game.board = chess();
+    // Running flag
     current_game.running = 1;
+    // Set Round
     current_game.current_round = round;
+    // Set PGN headers
     current_game.board.header('White', p1.name, 'Black', p2.name, 'Date', today, 'Round', current_game.current_round.toString());
+    // Start the game loop
     gameLoop();
 }
 
+// Requirements
 var chess = require('chess.js').Chess;
 var moment = require('moment');
 var fs = require('fs');
+
+// Date
 var date = moment();
 var today = date.format('DD-MM-YYYY');
+
+// Load config JSON
 var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+// Init vars
 var current_game;
 var round;
+
+// Create and init objects for players
 var p1 = Object.create(ai);
 var p2 = Object.create(ai);
 p1.path = config.p1.path;
@@ -261,4 +297,5 @@ p2.path = config.p2.path;
 p1.name = config.p1.name;
 p2.name = config.p2.name;
 
+// Start it up!
 play(1);
